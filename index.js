6820a03e9ee0adab42059d9b3aeac5dc68f7e639
pooblow02 <40965788+pooblow02@users.noreply.github.com>
2018-10-24 19:33:28 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+
+bot.on('message', function(message){
+    if(message.content == 'Register')
+    {
+        message.reply('Hello there if you want to join 100C, You must ask Co-Owner or higher and try out for the team! Thank you for Applying for 100C We appreciate it.');
+    }   
+ bot.on == true
+}
+    );
+
+
+bot.login('NTAzOTk1MDIzNzA5MTc1ODE4.Dq-p-A.pTloDlt5_TkvXR2MX1Qb1y3Dve0');
